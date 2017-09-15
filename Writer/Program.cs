a9dc0249b565ba8c7e1876c0d642ee72a7e7c9c1
Jef King <jefkin@microsoft.com>
2017-09-15 11:50:59 -0700

    init

---
+++ Writer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
